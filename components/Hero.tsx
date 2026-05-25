75fc892ce884652709ea054f7ae848391aad6ef6
Shivamjat7 <shivamjat531@gmail.com>
2026-05-25 14:29:41 +0530

    hero section part-1 done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,64 @@
-import React from 'react'
+"use client";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/all";
+import Image from "next/image";
+import React from "react";
+
 
 export default function Hero() {
+    useGSAP(()=>{
+        const heroSplit = new SplitText('.title',{  type:'chars,words' });
+        const paragraphSplit = new SplitText('.subtitle',{  type:'lines' });
+
+        heroSplit.chars.forEach((char)=>char.classList.add('text-gradient'));
+        gsap.from(heroSplit.chars,{
+            yPercent:100,
+            duration:1,
+            ease:'expo.out',
+            stagger:0.06
+        })
+        gsap.from(paragraphSplit.lines,{
+            opacity:0,
+            yPercent:100,
+            duration:1.8,
+            ease:'expo.out',
+            stagger:0.06,
+            delay:1,
+        })
+    },[]);
+
   return (
-    <section id="hero" className='noisy'></section>
-  )
+    <section id="hero" className="noisy">
+      <h1 className="title uppercase">Shivam Juice</h1>
+      <Image
+        className="w-auto h-auto left-leaf"
+        src={"/images/hero-left-leaf.png"}
+        alt="left leaf"
+        height={200}
+        width={200}
+      />
+      <Image
+        className="w-auto h-auto right-leaf"
+        src={"/images/hero-right-leaf.png"}
+        alt="left right"
+        height={200}
+        width={200}
+      />
+      <div className="body">
+        <div className="content">
+          <div className="space-y-5 hidden md:block">
+            <p>Cool. Crisp. Classic.</p>
+            <p className="subtitle">
+              Sip the Spirit <br /> of summer
+            </p>
+          </div>
+          <div className="view-cocktails">
+            <p className="subtitle">Ganne ka juice pilo bada glass 20 rupay jaldi yahan aayiye </p>
+            <a href="#cocktails">View Cocktails</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,55 @@
-import React from 'react'
-import {navLinks} from '../constants/constants'
-import Image from 'next/image';
-import { useGSAP } from '@gsap/react';
-import { gsap } from 'gsap';
+"use client";
+import { navLinks } from "../constants/constants";
+import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 export default function Navbar() {
-    useGSAP(()=>{
-        const navTween = gsap.timeline({
-            scrollTrigger:{
-                trigger:'nav',
-                start:'bottom top'
-            }
-        })
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
 
-        navTween.fromTo('nav',{
-            backgroundColor:'transparent'
-        },
-    {
-        backgroundColor:'#00000050',
-        backgroundFilter:'blur(10px)',
-        duration:1,
-        ease:'power1.inOut'
-    })
-    })
+    navTween.fromTo(
+      "nav",
+      {
+        backgroundColor: "transparent",
+      },
+      {
+        backgroundColor: "#00000030",
+        backdropFilter: "blur(2px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'#hero',
+            start:'top top',
+            end:'bottom top',
+            scrub:true,
+
+        }
+    }).to('.right-leaf',{y:200},0)
+    .to('.left-leaf',{y:-200},0)
+  },[]);
   return (
     <nav>
-        <div>
-            <a href="#home" className='flex items-center gap-2'>
-                <Image alt='logo' src={'/images/logo.png'} width={20} height={20}/>
-                <p>Velvet Pour</p>
-            </a>
-            <ul>
-                {navLinks.map((link)=>(
-                    <li key={link.id}>{link.title}</li>
-                ))}
-            </ul>
-
-        </div>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <Image alt="logo" src={"/images/logo.png"} width={20} height={20} />
+          <p>Velvet Pour</p>
+        </a>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>{link.title}</li>
+          ))}
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
